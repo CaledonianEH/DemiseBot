ecd5c0723b84547ed8b983ef2d09b1f2f2cb67d3
CaledonianEH <62163840+CaledonianEH@users.noreply.github.com>
2021-08-07 12:47:23 -0700

    testing

--- src/main/java/xyz/caledonian/commands/developer/EvalCommand.java
+++ src/main/java/xyz/caledonian/commands/developer/EvalCommand.java
@@ -72,16 +72,15 @@ public class EvalCommand extends ListenerAdapter {
                     engine.put("channel", e.getChannel());
 
                     try{
-//                        Object out = engine.eval(
-//                                "(function() {" +
-//                                        "with (imports) {" +
-//                                        eval) +
-//                                        "}" +
-//                                        "})();";
+                        Object out = engine.eval(
+                                "(function() {" +
+                                        "with (imports) {" +
+                                        eval) + "}"
+                                        + "});";
 
-                        Object out = engine.eval(eval);
+                        //Object out = engine.eval(eval);
 
-                        e.replyEmbeds(evalSuccess(eval, out.toString()).build()).queue();
+                        e.replyEmbeds(evalSuccess(eval, out == null ? "Empty return" : out.toString()).build()).queue();
                     }catch (Exception ex){
                         ex.printStackTrace();
                         e.replyEmbeds(PremadeEmbeds.error(ex.getMessage()).build()).queue();
