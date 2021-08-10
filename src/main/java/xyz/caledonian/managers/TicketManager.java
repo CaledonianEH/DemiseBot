d70930ad7d545a27581b68b1d47622ff81835412
CaledonianEH <62163840+CaledonianEH@users.noreply.github.com>
2021-08-10 12:04:34 -0700

    Edited emoji

--- src/main/java/xyz/caledonian/managers/TicketManager.java
+++ src/main/java/xyz/caledonian/managers/TicketManager.java
@@ -47,10 +47,10 @@ public class TicketManager {
                 ticket.getId())).build()).queue();
 
         ticket.sendMessage("@here").setEmbeds(PremadeEmbeds.success("Thank you for creating a ticket! Our team should be here as soon as possible.\n\nYou can help us out by informing what you're in need of!").build())
-                .setActionRow(Button.danger("ticketCloseBtn", "Close ticket")
+                .setActionRow(Button.secondary("ticketCloseBtn", "Close ticket")
                         .withEmoji(Emoji.fromMarkdown(main.getConfig().getJSONObject("emotes").getString("close")))
                         ).queue();
-        // .withEmoji(Emoji.fromMarkdown(main.getConfig().getJSONObject("emotes").getString("close"))
+        return;
     }
 
     @SneakyThrows
