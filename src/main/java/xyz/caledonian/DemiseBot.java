39e155dfe72c736ebd03562f0718774c2be4c07e
Caledonian <caledonianeh@gmail.com>
2021-09-04 22:11:27 -0700

    Update DemiseBot.java

--- src/main/java/xyz/caledonian/DemiseBot.java
+++ src/main/java/xyz/caledonian/DemiseBot.java
@@ -176,6 +176,3 @@ public class DemiseBot {
         System.out.printf("    - Current shard: %s", jda.getShardInfo().getShardId());
         System.out.printf("=============================================");
     }
-}
-
-// need to redeploy
